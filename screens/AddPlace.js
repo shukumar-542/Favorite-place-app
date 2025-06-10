ef0b4be6c5671f3d751f773c68308941286c4144
shukumar-542 <shukumar542@gmail.com>
2025-06-10 15:26:43 +0600

    Implement react navigation

---
+++ screens/AddPlace.js
@@ -0,0 +1,9 @@
+import { Text } from 'react-native'
+
+const AddPlace = () => {
+  return (
+    <Text>AddPlace</Text>
+  )
+}
+
+export default AddPlace
---
+++ screens/AllPlaces.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const AllPlaces = () => {
+  return (
+    <Text>
+       All Places 
+    </Text>
+  )
+}
+
+export default AllPlaces
