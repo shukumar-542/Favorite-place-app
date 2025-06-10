8851298bbe7a240d511167dffb185fed6956ea3c
shukumar-542 <shukumar542@gmail.com>
2025-06-10 16:01:12 +0600

    Implement custom button

--- App.js
+++ App.js
@@ -1,32 +1,36 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import AllPlaces from './screens/AllPlaces';
-import AddPlace from './screens/AddPlace';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
 
-
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-    <StatusBar style='dark' />
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='AllPlaces' component={AllPlaces} />
-        <Stack.Screen name='AddPlaces' component={AddPlace} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  color={tintColor}
+                  size={30}
+                  icon="add"
+                  onPress={() => navigation.navigate("AddPlaces")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen name="AddPlaces" component={AddPlace} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/UI/IconButton.js
@@ -0,0 +1,23 @@
+import {  Pressable, StyleSheet } from 'react-native'
+import {Ionicons} from  "@expo/vector-icons"
+const IconButton = ({icon , color , size , onPress}) => {
+  return (
+    <Pressable style={({pressed})=>[styles.button , pressed && styles.pressed]} onPress={onPress}>
+        <Ionicons name={icon} color={color} size={size}/>
+    </Pressable>
+  )
+}
+
+export default IconButton
+
+const styles = StyleSheet.create({
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 6,    
+    marginRight: 10, 
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
